559b0ef28f338a56157b1fc81384eeb0eca30eac
Mortimer Jarasemy <marcoscolladosegura@ied.edu>
2020-08-31 09:10:03 +0200

    this commit adds proper functionality to pagination and limits the inputs in it respectively

--- src/00-components/PaginationController/PaginationControl.style.js
+++ src/00-components/PaginationController/PaginationControl.style.js
@@ -1,28 +1,21 @@
 import styled from 'styled-components'
+import {whiteColor} from '../../07-styles/index.style';
 
 export const StyledPaginationController = styled.div`
-height:34px;
-width:auto;
-display:flex;
-flex-wrap:nowrap;
-margin-left:32px;
-line-height:3px;
-align-items:center;
-button{
-  border-radius:3px;
-  background:#87CEFA;
-  outline:none;
-  border:none;
-  margin-left:6px;
-}
-button:hover{
-  background:pink;
-  cursor: pointer !important;
+height:30px;
+box-shadow: 0 1px 6px 0 rgba(32,32,36,0.30);
+border-radius: 30px;
+background: ${whiteColor};
+.pagesWrapper{
+  line-height: 20px;
+  display: flex;
+  justify-content: space-between;
+  padding: 4px 24px;
+  width: 156px;
 }
 p{
   text-transform:uppercase;
   font-weight:600;
-  margin-left:30px;
   justify-content:flex-start;
 }
 `
--- src/00-components/PaginationController/index.js
+++ src/00-components/PaginationController/index.js
@@ -3,10 +3,11 @@ import React,{useCallback} from 'react';
 import { useDispatch } from 'react-redux';
 //actions
 import {setCurrentPageAction} from '../../02-actions/index';
+import { StyledPaginationController } from './PaginationControl.style';
 
 
 const renderBefore = (currentPage,numberOfTotalPages, setPage) => {
-  if(currentPage > numberOfTotalPages){
+  if(currentPage >= numberOfTotalPages){
     return (<button onClick={() => setPage(currentPage-1)}>Before</button>)
   }
 }
@@ -17,26 +18,37 @@ const renderAfter = (currentPage,numberOfTotalPages,setPage) => {
   }
 }
 
+const handleChange = (evt,setPage,totalPages,currentPage) => {
+  if(totalPages >= evt.target.value && evt.target.value > 0){
+    setPage(Number(evt.target.value))
+  }
+  return currentPage;
+}
+
 const renderTools = (currentPage, totalPages, setPage) => {
   return (
-  <>
+  <div className='pagesWrapper'>
     {renderBefore(currentPage, totalPages, setPage)}
-      <label>
-        Page
-        <input type='number' min='0' max={totalPages} placeholder={currentPage} onChange={(evt) => setPage(Number(evt.target.value))}/>
-      </label>
+      <label> Page </label>
+        <input 
+          type='number' 
+          min='1' 
+          max={totalPages} placeholder={currentPage} 
+          onChange={(evt) => handleChange(evt,setPage,totalPages,currentPage)}
+        />
     {renderAfter(currentPage, totalPages, setPage)}
-  </>
+  </div>
   )
 }
 
-const PaginationController = ({data,currentPage}) => {
+const PaginationController = ({data,currentPage,loaderFunction}) => {
   const dispatch = useDispatch();
   const setPage = useCallback((numberPage) => {
     dispatch(setCurrentPageAction(numberPage));
-  }, [dispatch]);
+    loaderFunction(true);
+  }, [dispatch, loaderFunction]);
   return(
-    <>{data.page_number === 1 ? '' : renderTools(currentPage, data.total_pages, setPage)}</>
+    <StyledPaginationController>{data.page_number < 0 ? '' : renderTools(currentPage, data.total_pages, setPage)}</StyledPaginationController>
   )
 }
 
